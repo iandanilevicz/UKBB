79625c8ef36440444e94ca1c67831a92782c033d
Ian Danilevicz <43249776+iandanilevicz@users.noreply.github.com>
2023-11-03 17:31:50 +0100

    Update 0-0_GGIR_extraction.R
    I adjusted it according to Vincent's Github. Now we have separate users and studies, and I call the Verisense function from the source.

--- 0-0_GGIR_extraction.R
+++ 0-0_GGIR_extraction.R
@@ -4,170 +4,94 @@
 #         Ian M. Danilevicz, INSERM
 # Date: 2023
 # R script to process accelerometer data from UK biobank
-# ========================================================= #
-
 # remotes::install_github("wadpac/GGIR")
+# ========================================================= #
 library(GGIRread)
 library(GGIR)
 print(packageVersion("GGIR"))      # Used version 3.0.0
 print(packageVersion("GGIRread"))  # Used version 0.3.1
 
-datadir = "/home1/USERS/DATA_UKBB/TEST5"
-outputdir = "/home1/USERS/DATA_UKBB/RESULTS"
-maxNcores = 35  # this is for a server, standard PC has only 8
-chunksize = 0.7 # it can be between 0.5 and 1
+user = "Ian"
+study  = "UKBiobank"
 
-# Generic script for using GGIR in the ERC project
-#=====================================================================================
-# load GGIR or functions directly
-# library(GGIR)
-# # Load function files directly
-# fns = dir("D:/Code/GGIR/R", full.names = TRUE) # creating list of filenames of scriptfiles to load
-# for (i in fns) source(i)
-
-# Load step detection function:
-verisense_count_steps <- function(input_data=runif(500,min=-1.5,max=1.5), coeffs=c(0,0,0)) {
-  # by Matthew R Patterson, mpatterson@shimmersensing.com
-  ## Find peaks of RMS acceleration signal according to Gu et al, 2017 method
-  # This method is based off finding peaks in the summed and squared acceleration signal
-  # and then using multiple thresholds to determine if each peak is a step or an artefact.
-  # An additional magnitude threshold was added to the algorithm to prevent false positives 
-  # in free living data. 
-  #
-  # returns sample location of each step
-  fs = 15 # temporary for now, this is manually set
-  acc <- sqrt(input_data[,1]^2 + input_data[,2]^2 + input_data[,3]^2)
+# Parameters that are user specific:
+if (user == "Vincent") {
+  local_datadir = "D:/whitehall"
+  local_outputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Whitehall"
+  verisensedir = "D:/Code/erc-accelerometry/verisense_count_steps.R"
+  # verisensedir = "verisense_count_steps.R"
+  mode = 6
+  do.parallel = TRUE
+  f0 = 1
+  f1 = 100
+  do.report = c(5, 6) #c() #c(2, 4, 5, 6)
+  maxNcores = 12
+  fns = dir("D:/Code/GGIR/R", full.names = TRUE) # creating list of filenames of scriptfiles to load
+  for (i in fns) source(i)
   
-  if (sd(acc) < 0.025) {
-    # acceleration too low, no steps
-    num_seconds = round(length(acc) / fs)
-    steps_per_sec = rep(0,num_seconds)
-  } else {
-    # Search for steps
-    # Thresholds
-    k <- coeffs[[1]]
-    period_min <- coeffs[[2]]
-    period_max <- coeffs[[3]]
-    sim_thres <- coeffs[[4]]   # similarity threshold
-    cont_win_size <- coeffs[[5]]  # continuity window size
-    cont_thres <- coeffs[[6]]     # continuity threshold
-    var_thres <- coeffs[[7]]  # variance threshold
-    mag_thres <- coeffs[[8]]
-    
-    # find the peak rms value is every range of k
-    half_k <- round(k/2)
-    segments <- floor(length(acc) / k)
-    peak_info <- matrix(NA,nrow=segments,ncol=5)
-    # peak_info[,1] - peak location
-    # peak_info[,2] - acc magnitude
-    # peak_info[,3] - periodicity (samples)
-    # peak_info[,4] - similarity
-    # peak_info[,5] - continuity
-    
-    # for each segment find the peak location
-    for (i in 1:segments) {
-      start_idx <- (i-1) * k + 1
-      end_idx <- start_idx + (k-1)
-      tmp_loc_a <- which.max(acc[start_idx:end_idx])
-      tmp_loc_b <- (i-1) * k + tmp_loc_a
-      # only save if this is a peak value in range of -k/2:+K/2
-      start_idx_ctr <- tmp_loc_b - half_k
-      if (start_idx_ctr < 1) {
-        start_idx_ctr <- 1
-      }
-      end_idx_ctr <- tmp_loc_b + half_k
-      if (end_idx_ctr > length(acc)) {
-        end_idx_ctr <- length(acc)
-      }
-      check_loc <- which.max(acc[start_idx_ctr:end_idx_ctr])
-      if (check_loc == (half_k + 1)) {
-        peak_info[i,1] <- tmp_loc_b
-        peak_info[i,2] <- max(acc[start_idx:end_idx])
-      }
-    }
-    peak_info <- peak_info[is.na(peak_info[,1])!=TRUE,] # get rid of na rows
-    
-    # filter peak_info[,2] based on mag_thres
-    peak_info <- peak_info[peak_info[,2] > mag_thres,]
-    if (length(peak_info) > 10) {  # there must be at least two steps
-      num_peaks <- length(peak_info[,1])
-      
-      no_steps = FALSE
-      if (num_peaks > 2) {
-        # Calculate Features (periodicity, similarity, continuity)
-        peak_info[1:(num_peaks-1),3] <- diff(peak_info[,1]) # calculate periodicity
-        peak_info <- peak_info[peak_info[,3] > period_min,] # filter peaks based on period_min
-        peak_info <- peak_info[peak_info[,3] < period_max,]   # filter peaks based on period_max 
-      } else {
-        no_steps = TRUE
-      }
-    } else {
-      no_steps = TRUE
-    }
-    
-    if ( length(peak_info)==0 || length(peak_info) == sum(is.na(peak_info)) || no_steps == TRUE) {
-      # no steps found
-      num_seconds = round(length(acc) / fs)
-      steps_per_sec = rep(0,num_seconds)
-    } else {
-      # calculate similarity
-      num_peaks <- length(peak_info[,1])
-      peak_info[1:(num_peaks-2),4] <- -abs(diff(peak_info[,2],2)) # calculate similarity
-      peak_info <- peak_info[peak_info[,4] > sim_thres,]  # filter based on sim_thres
-      peak_info <- peak_info[is.na(peak_info[,1])!=TRUE,] # previous statement can result in an NA in col-1
-      
-      # calculate continuity
-      if (length(peak_info[,3]) > 5) {
-        end_for <- length(peak_info[,3])-1
-        for (i in cont_thres:end_for) {
-          # for each bw peak period calculate acc var
-          v_count <- 0 # count how many windows were over the variance threshold
-          for (x in 1:cont_thres) {
-            if (var(acc[peak_info[i-x+1,1]:peak_info[i-x+2,1]]) > var_thres) {
-              v_count = v_count + 1
-            }
-          }
-          if (v_count >= cont_win_size) {
-            peak_info[i,5] <- 1 # set continuity to 1, otherwise, 0
-          } else {
-            peak_info[i,5] <- 0
-          }
-        }
-      } 
-      peak_info <- peak_info[peak_info[,5]==1,1] # continuity test - only keep locations after this
-      peak_info <- peak_info[is.na(peak_info)!=TRUE] # previous statement can result in an NA in col-1
-      
-      if (length(peak_info)==0) {
-        # no steps found
-        num_seconds = round(length(acc) / fs)
-        steps_per_sec = rep(0,num_seconds)
-      } else {
-      
-        # debug plot
-        # is_plot = F
-        # if (is_plot) {
-        #   library(ggplot2)
-        #   library(plotly)
-        #   acc.df <- data.frame(acc=acc, det_step=integer(length(acc)))
-        #   acc.df$det_step[peak_info] <- 1  # to plot annotations, prepare a 0/1 column on dataframe
-        #   acc.df$idx <- as.numeric(row.names(acc.df))
-        #   pl <- ggplot(data=acc.df,aes(x=idx,y=acc)) 
-        #   pl2 <- pl + geom_line()
-        #   pl3 <- pl2 + geom_point(data=subset(acc.df,det_step==1),aes(x=idx,y=acc),color='red',size=1,alpha=0.7)
-        #   pl4 <- ggplotly(pl3)
-        #   print(pl4)  
-        # }
-        
-        # for GGIR, output the number of steps in 1 second chunks
-        start_idx_vec <- seq(from=1,to=length(acc),by=fs)
-        steps_per_sec <- table(factor(findInterval(peak_info, start_idx_vec), levels = seq_along(start_idx_vec)))
-        steps_per_sec <- as.numeric(steps_per_sec)
-      }
-    }
-  }
-  return(steps_per_sec)
+} else if (user == "Ian") {
+  local_datadir = "/home1/USERS/DATA_UKBB/TEST5"
+  local_outputdir = "/home1/USERS/DATA_UKBB/RESULTS"
+  verisensedir = "/home1/USERS/meneghel/verisense_count_steps.R"
+  mode = 1:2
+  do.parallel = TRUE
+  f0 = 1
+  f1 = 0
+  do.report = 2
+  maxNcores = 36 # this is for a server, standard PC has only 8
+  library(GGIR)
 }
 
+# Parameters that are study specific
+if (study == "UKBiobank") {
+  timewindow = "OO"
+  strategy = 1
+  maxdur = 7
+  datadir = local_datadir
+  outputdir = local_outputdir
+  studyname = "binfiles"
+  loglocation = NULL
+  data_cleaning_file = NULL
+  idloc = 2
+  excludefirstlast = FALSE
+  LUX_cal_constant = NULL
+  LUX_cal_exponent = NULL
+  desiredtz = "Europe/London"
+  part6Window = c("O1", "O-1")
+} else if (study == "Whitehall") {
+  timewindow = "WW"
+  strategy = 2
+  maxdur = 9
+  datadir = local_datadir
+  outputdir = local_outputdir
+  studyname = "whitehall"
+  loglocation = paste0(outputdir, "/output_whitehall/sleeplog_corrected_v9.csv")
+  data_cleaning_file = paste0(outputdir, "/output_whitehall/data_cleaning_file_v7.csv")
+  idloc = 2
+  excludefirstlast = TRUE
+  LUX_cal_constant = 1.173
+  LUX_cal_exponent = 0.0193
+  part6Window = c("W1", "W-1")
+  desiredtz = "Europe/London"
+} else if (study == "Clinic") {
+  timewindow = "WW"
+  strategy = 2
+  maxdur = 9
+  datadir = "D:/binfiles"
+  outputdir = "D:/analysis"
+  studyname = "binfiles"
+  loglocation = ".../sleeplog_corrected_v9.csv"
+  data_cleaning_file = ".../data_cleaning_file_v7.csv"
+  idloc = 2
+  excludefirstlast = TRUE
+  LUX_cal_constant = NULL
+  LUX_cal_exponent = NULL
+  part6Window = c("start", "end")
+  desiredtz = "Europe/Paris"
+}
+
+# Load step detection function:
+source(verisensedir)
 myfun = list(FUN = verisense_count_steps,
              parameters = c(4, 4, 20, -1.0, 4, 4, 0.01, 1.25), # updated based on Rowlands et al Stepping up with GGIR 2022
              expected_sample_rate = 15,
@@ -180,130 +104,93 @@ myfun = list(FUN = verisense_count_steps,
              timestamp = F,
              reporttype = "event")
 
-study  = "UKBiobank"
-
-if (study == "UKBiobank") {
-  timewindow = "OO" # as the device comes from post, there is no "clinic influence"
-  strategy = 1
-  maxdur = 7
-  datadir = datadir
-  outputdir = outputdir
-  studyname = "binfiles"
-  loglocation = NULL
-  data_cleaning_file = NULL
-  idloc = 2
-  excludefirstlast = FALSE
-  LUX_cal_constant = NULL
-  LUX_cal_exponent = NULL
-  desiredtz = "Europe/London"
-} else if (study == "Whitehall") {
-  timewindow = "WW"
-  strategy = 2
-  maxdur = 9
-  datadir = "D:/binfiles"
-  outputdir = "D:/analysis"
-  studyname = "binfiles"
-  loglocation = ".../sleeplog_corrected_v9.csv"
-  data_cleaning_file = ".../data_cleaning_file_v7.csv"
-  idloc = 2
-  excludefirstlast = TRUE
-  LUX_cal_constant = 1.173
-  LUX_cal_exponent = 0.0193
-  desiredtz = "Europe/London"
-} else if (study == "Clinic") {
-  timewindow = "WW"
-  strategy = 2
-  maxdur = 9
-  datadir = datadir
-  outputdir = outputdir
-  studyname = "binfiles"
-  loglocation = ".../sleeplog_corrected_v9.csv"
-  data_cleaning_file = ".../data_cleaning_file_v7.csv"
-  idloc = 2
-  excludefirstlast = TRUE
-  LUX_cal_constant = NULL
-  LUX_cal_exponent = NULL
-  desiredtz = "Europe/Paris"
-}
-
-g.shell.GGIR(#-------------------------------
-             # General parameters
-             #-------------------------------
-             myfun = myfun,
-             mode = c(1:5),
-             datadir = datadir,
-             outputdir = outputdir,
-             studyname = studyname,
-             do.report = c(2, 4, 5),
-             f0 = 1,
-             f1 = 0,
-             overwrite = TRUE,
-             do.parallel = TRUE,
-             maxNcores = maxNcores,
-             idloc = idloc,
-             print.filename = TRUE,
-             storefolderstructure = TRUE,
-             data_cleaning_file = data_cleaning_file,
-             desiredtz = desiredtz, 
-             #-------------------------------
-             # Part 1 parameters:
-             #-------------------------------
-             windowsizes = c(5, 900, 3600),
-             do.enmo = TRUE,
-             do.anglez = TRUE,
-             chunksize = chunksize,
-             printsummary = TRUE,
-             #-------------------------------
-             # Part 2 parameters:
-             #-------------------------------
-             strategy = strategy,
-             maxdur = maxdur,
-             winhr = c(5, 10),
-             ilevels = c(seq(0, 400, by = 50), 8000),
-             iglevels = TRUE,
-             mvpathreshold = c(100, 120),
-             IVIS_windowsize_minutes = 60,
-             IVIS.activity.metric = 2,
-             #-------------------------------
-             # Part 3 parameters:
-             #-------------------------------
-             do.part3.pdf = TRUE, 
-             #-------------------------------
-             # Part 4 parameters:
-             #-------------------------------
-             excludefirstlast = excludefirstlast, # Exclude first and last night for sleep analysis?
-             def.noc.sleep = 1,
-             loglocation = loglocation,
-             outliers.only = TRUE,
-             criterror = 4,
-             colid = 1,
-             coln1 = 2,
-             #-------------------------------
-             # Part 5 parameters:
-             #-------------------------------
-             # Threshold for intensity levels
-             threshold.lig = c(40, 45),
-             threshold.mod = c(100, 110),
-             threshold.vig = c(400),
-             boutcriter = 0.8,
-             boutcriter.in = 1,
-             boutcriter.lig = 1,
-             boutcriter.mvpa = 1,
-             # duration of bouts to be calculated
-             boutdur.in = c(10, 30), 
-             boutdur.lig = c(10),
-             boutdur.mvpa = c(10),
-             timewindow = timewindow,
-             save_ms5rawlevels = TRUE,
-             includedaycrit.part5 = 2 / 3,
-             minimum_MM_length.part5 = 23,
-             frag.metrics = "all",
-             LUX_cal_constant = LUX_cal_constant,
-             LUX_cal_exponent = LUX_cal_exponent,
-             LUX_day_segments = c(4, 8, 12, 16, 20, 24),
-             part5_agg2_60seconds = TRUE,
-             cosinor = TRUE,
-             #-----------------------------------
-             # pdf report generation
-             visualreport = FALSE)
+GGIR(#-------------------------------
+     # General parameters
+     #-------------------------------
+     myfun = myfun,
+     mode = mode,
+     datadir = datadir,
+     outputdir = outputdir,
+     studyname = studyname,
+     do.report = do.report,
+     f0 = f0,
+     f1 = f1,
+     overwrite = TRUE,
+     do.parallel = do.parallel,
+     maxNcores = maxNcores,
+     idloc = idloc,
+     print.filename = TRUE,
+     storefolderstructure = TRUE,
+     data_cleaning_file = data_cleaning_file,
+     desiredtz = desiredtz, 
+     #-------------------------------
+     # Part 1 parameters:
+     #-------------------------------
+     windowsizes = c(5, 900, 3600),
+     do.enmo = TRUE,
+     do.anglez = TRUE,
+     chunksize = 1,
+     printsummary = TRUE,
+     #-------------------------------
+     # Part 2 parameters:
+     #-------------------------------
+     strategy = strategy,
+     maxdur = maxdur,
+     winhr = c(5, 10),
+     ilevels = c(seq(0, 400, by = 50), 8000),
+     iglevels = TRUE,
+     mvpathreshold = c(100, 120),
+     IVIS_windowsize_minutes = 60,
+     IVIS.activity.metric = 2,
+     do.part2.pdf = FALSE,
+     #-------------------------------
+     # Part 3 parameters:
+     #-------------------------------
+     do.part3.pdf = FALSE, 
+     #-------------------------------
+     # Part 4 parameters:
+     #-------------------------------
+     excludefirstlast = excludefirstlast, # Exclude first and last night for sleep analysis?
+     def.noc.sleep = 1,
+     loglocation = loglocation,
+     outliers.only = TRUE,
+     criterror = 4,
+     colid = 1,
+     coln1 = 2,
+     #-------------------------------
+     # Part 5 parameters:
+     #-------------------------------
+     # Threshold for intensity levels
+     threshold.lig = c(40, 45),
+     threshold.mod = c(100, 110),
+     threshold.vig = c(400),
+     boutcriter = 0.8,
+     boutcriter.in = 1,
+     boutcriter.lig = 1,
+     boutcriter.mvpa = 1,
+     # duration of bouts to be calculated
+     boutdur.in = c(10, 30), 
+     boutdur.lig = c(10),
+     boutdur.mvpa = c(10),
+     timewindow = timewindow,
+     # saving output
+     save_ms5rawlevels = TRUE,
+     save_ms5raw_format = "RData",
+     includedaycrit.part5 = 2 / 3,
+     minimum_MM_length.part5 = 23,
+     frag.metrics = "all",
+     LUX_cal_constant = LUX_cal_constant,
+     LUX_cal_exponent = LUX_cal_exponent,
+     LUX_day_segments = c(4, 8, 12, 16, 20, 24),
+     part5_agg2_60seconds = TRUE,
+     cosinor = TRUE,
+     #-------------------------------
+     # Part 6 parameters:
+     #-------------------------------
+     part6_threshold_combi = "40_100_400",
+     part6CR = TRUE,
+     part6Window = part6Window,
+     #-----------------------------------
+     # pdf report generation
+     visualreport = FALSE)
 
